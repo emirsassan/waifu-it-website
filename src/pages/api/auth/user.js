6db92ffcfb6bfc66f7e68b6e98d71fbe2f84d96e
kyrea <notaeryk@gmail.com>
2024-11-25 01:36:36 +0530

    Refactor user handling API logic

--- src/pages/api/auth/user.js
+++ src/pages/api/auth/user.js
@@ -1,49 +1,46 @@
-// api/user.js
 import axios from "axios";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { id, email, token, access_token } = req.body;
-    const apiKey = process.env.ACCESS_KEY; // Replace 'API_KEY' with the actual environment variable name that holds your API key
+  const { method, body } = req;
+  const { id, email, token, access_token } = body;
+  const apiKey = process.env.ACCESS_KEY;
 
-    if (!id && !token) {
-      return res
-        .status(400)
-        .json({ message: "User ID and Token missing in the request body" });
+  if (method !== "POST") {
+    return res.status(405).end(); // Method Not Allowed
+  }
+
+  if (!id && !token) {
+    return res.status(400).json({ message: "User ID and Token are required" });
+  }
+
+  try {
+    let api_response;
+
+    if (email) {
+      // Call GET endpoint
+      api_response = await axios.post(
+        `${process.env.API_URL}/user`,
+        {
+          id,
+          email,
+          "access-token": access_token,
+        },
+        { headers: { key: apiKey } }
+      );
+    } else if (token) {
+      // Call POST endpoint
+      api_response = await axios.post(
+        `${process.env.API_URL}/user`,
+        { id, token },
+        { headers: { key: apiKey } }
+      );
     }
 
-    try {
-      if (email) {
-        const api_response = await axios.get(`${process.env.API_URL}/user`, {
-          headers: {
-            key: apiKey,
-            id,
-            email,
-            "access-token": access_token,
-          },
-        });
-        res.json(api_response.data);
-      } else if (token) {
-        const api_response = await axios.post(
-          `${process.env.API_URL}/user`,
-          {
-            id,
-            token,
-            email,
-          },
-          {
-            headers: {
-              key: apiKey,
-            },
-          }
-        );
-        res.json(api_response.data);
-      }
-    } catch (error) {
-      console.error("Error performing the request");
-      res.status(500).json({ error: "Error performing the request" });
-    }
-  } else {
-    return res.status(405).end(); // Method Not Allowed for other request types
+    return res.json(api_response.data);
+  } catch (error) {
+    console.error("API request error:", error.message);
+    return res
+      .status(error.response?.status || 500)
+      .json({ error: error.response?.data || "Internal Server Error" });
   }
 }
